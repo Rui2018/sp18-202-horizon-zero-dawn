c0d3b796664eb52aa42866082250f508c9eb7b89
Rui2018 <ruiguoapply2017@gmail.com>
2018-04-24 16:34:47 -0700

    Recover

---
+++ Rui_Scenario-3/CatchStarState.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class CatchStarState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CatchStarState implements State 
+{
+    // instance variables - replace the example below with your own
+     public void act_jump(){
+        System.out.println("");
+    }
+    
+    public void act_shoot(){}
+    public void act_chatchstar(){}
+}
