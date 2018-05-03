2fead9990d7a50b40392a78ef8fb384a009aa55b
Rui2018 <ruiguoapply2017@gmail.com>
2018-05-03 10:00:21 -0700

    complete factory pattern

---
+++ src/StillStarCreator.java
@@ -0,0 +1,12 @@
+/**
+ * Write a description of class StillStarCreator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StillStarCreator extends Creator 
+{
+   public Stars getStars(){
+        return new stillStar();
+    }
+}
